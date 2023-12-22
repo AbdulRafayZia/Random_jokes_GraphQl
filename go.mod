df4d13636b3fade4f7b231825630bbaeba3d84ed
Abdul Rafay Zia <rafayzia3690@gmail.com>
2023-12-22 12:03:27 +0500

    can not get response

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/AbdulRafayZia/graphql
 
 go 1.21.1
 
-require github.com/graphql-go/graphql v0.8.1
+require (
+	github.com/graphql-go/graphql v0.8.1
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -2,40 +2,102 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
+	"math/rand"
+	"net/http"
+	"time"
 
+	"github.com/AbdulRafayZia/graphql/db"
 	"github.com/graphql-go/graphql"
+	_ "github.com/lib/pq"
+)
+
+// Joke represents the structure of a joke.
+type Joke struct {
+	ID   int    `json:"id"`
+	Joke string `json:"joke"`
+}
+
+var jokeType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Joke",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"joke": &graphql.Field{ // Update "content" to "joke"
+				Type: graphql.String,
+			},
+		},
+	},
+)
+
+var rootQuery = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "RootQuery",
+		Fields: graphql.Fields{
+			"joke": &graphql.Field{
+				Type: jokeType,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return GetRandomJoke(), nil
+				},
+			},
+		},
+	},
+)
+
+func GetRandomJoke() *Joke {
+	var jokes []Joke
+
+	db := db.OpenDB()
+
+	// Retrieve jokes from the database and populate the 'jokes' slice.
+	rows, err := db.Query("SELECT id, joke FROM jokes")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var joke Joke
+		err := rows.Scan(&joke.ID, &joke.Joke)
+		if err != nil {
+			log.Fatal(err)
+		}
+		jokes = append(jokes, joke)
+	}
+	if len(jokes) == 0 {
+		return nil
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	index := rand.Intn(len(jokes))
+	return &jokes[index]
+}
+
+var Schema, _ = graphql.NewSchema(
+	graphql.SchemaConfig{
+		Query: rootQuery,
+	},
 )
 
 func main() {
-	// Schema
-	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world", nil
-			},
-		},
-	}
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, err := graphql.NewSchema(schemaConfig)
-	if err != nil {
-		log.Fatalf("failed to create new schema, error: %v", err)
-	}
+	// Connect to PostgreSQL database.
 
-	// Query
-	query := `
-		{
-			hello
+	// Set up the HTTP handler for GraphQL queries.
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		result := graphql.Do(graphql.Params{
+			Schema:        Schema,
+			RequestString: r.URL.Query().Get("query"),
+		})
+		if len(result.Errors) > 0 {
+			log.Printf("GraphQL query errors: %v", result.Errors)
 		}
-	`
-	params := graphql.Params{Schema: schema, RequestString: query}
-	r := graphql.Do(params)
-	if len(r.Errors) > 0 {
-		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
-	}
-	rJSON, _ := json.Marshal(r)
-	fmt.Printf("%s \n", rJSON) // {"data":{"hello":"world"}}
+
+		json.NewEncoder(w).Encode(result)
+	})
+
+	// Start the server.
+	log.Println("Server is running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
